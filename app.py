3e866012b4fe6070c587d9df68b5fe2e1db3cdc1
Ubuntu <ubuntu@ip-172-31-27-29.ec2.internal>
2025-07-07 15:18:26 +0000

    Add Flask app and Dockerfile

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+import os
+import mysql.connector
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            database=os.getenv("DB_NAME")
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT NOW();")
+        result = cursor.fetchone()
+        return f"<h1>DB Connected. Time: {result[0]}</h1>"
+    except Exception as e:
+        return f"<h1>DB Connection Failed</h1><p>{e}</p>"
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
+
